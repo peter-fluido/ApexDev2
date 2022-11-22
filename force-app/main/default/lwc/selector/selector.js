fbe0af73c9523dbb41c793c30b23114ac21e1f22
Peter Friberg <peter.friberg@fluidogroup.com>
2022-11-22 17:06:11 +0100

    Added som css and wire of data.

--- force-app/main/default/lwc/selector/selector.js
+++ force-app/main/default/lwc/selector/selector.js
@@ -1,4 +1,9 @@
 import { LightningElement } from "lwc";
+// import { LightningElement, wire } from 'lwc';
+// import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+// import Id from '@salesforce/user/Id';
+// import NAME_FIELD from '@salesforce/schema/User.Name';
+// const fields = [NAME_FIELD];
 
 export default class Selector extends LightningElement {
     selectedProductId;
@@ -6,4 +11,11 @@ export default class Selector extends LightningElement {
     handleProductSelected(evt) {
         this.selectedProductId = evt.detail;
     }
+
+    // userId = Id;
+    // @wire(getRecord, { recordId: '$userId', fields })
+    // user;
+    // get name() {
+    //     return getFieldValue(this.user.data, NAME_FIELD);
+    // }
 }
